6681439aee47d0c873ffa7a0ed64017fecc04f15
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-04-29 12:08:20 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,15 +1,26 @@
 // use this to copy code snippets or use your browser's console
 
-let sally = { name: 'Sally' };
-
-function greet(customer) {
-    console.log(`Hi ${customer}, my name is ${this.name}!`);
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
 }
-
-let newGreet = greet.bind(sally); // newGreet is context-bound to sally
-
-newGreet('Bob');
-// Hi Bob, my name is Sally!
-
-greet('Bob');
-// Hi Bob, my name is !
+ 
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+ 
+    matchInterests(event) {
+        return event.keywords.some(function(word) {
+            return this.interests.includes(word);
+        });
+    }
+}
+ 
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+ 
+billy.matchInterests(freeMusic);
