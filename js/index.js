43bb165db8fb9c55bdae0d298d06122cfeef5e27
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-04-29 12:06:17 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // use this to copy code snippets or use your browser's console
 
+let sally = { name: 'Sally' };
 
 function greet(customerOne, customerTwo) {
     console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
