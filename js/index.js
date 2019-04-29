3fd30becc83066f3dd96fc7b091a550394055adb
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-04-29 12:07:04 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 // use this to copy code snippets or use your browser's console
 
 let sally = { name: 'Sally' };
-
-function greet(customerOne, customerTwo) {
-    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+ 
+function greet(customer) {
+    console.log(`Hi ${customer}, my name is ${this.name}!`);
 }
-
-greet.call(sally, 'Terry', 'George');
-
-greet.apply(sally, ['Terry', 'George']);
+ 
+let newGreet = greet.bind(sally); // newGreet is context-bound to sally
+ 
+newGreet('Bob');
+// Hi Bob, my name is Sally!
+ 
+greet('Bob');
+// Hi Bob, my name is !
