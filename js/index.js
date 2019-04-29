11bc20de9e168ab6f8c210b76c341af167afd77b
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-04-29 12:08:22 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,21 +6,21 @@ class Event {
         this.keywords = keywords;
     }
 }
- 
+
 class User {
     constructor(name, interests) {
         this.name = name;
         this.interests = interests;
     }
- 
+
     matchInterests(event) {
         return event.keywords.some(function(word) {
             return this.interests.includes(word);
         });
     }
 }
- 
+
 let billy = new User('billy', ['music', 'art', 'movies']);
 let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
- 
+
 billy.matchInterests(freeMusic);
